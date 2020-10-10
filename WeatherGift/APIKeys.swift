f77fe987956c886cf1d00f3ce21f0f27b31c6b90
Claudine Haigian <haigianc@bc.edu>
2020-10-10 14:52:29 -0400

    Through 6.4

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Claudine Haigian on 10/10/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyBG8V7H1McCHX9cuONrJ0QKlHfdCRLosr0"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Claudine Haigian on 10/9/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
